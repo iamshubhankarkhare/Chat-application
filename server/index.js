68958e4df8b2c6384874a6cc9094be32061b7040
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-05-19 01:20:02 +0530

    preproduction changes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-app.use(bodyParser.json());
 
 
 // app.use(router);
