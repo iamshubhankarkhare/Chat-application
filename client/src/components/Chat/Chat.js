c5a3d415993a80d64e97d97c48ad71a7b688dcc2
chiteshbansal <63744720+chiteshbansal@users.noreply.github.com>
2020-12-06 15:10:51 +0530

    styled the invite link button

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,37 +4,28 @@ import InfoBar from "../InfoBar/InfoBar";
 import Messages from "../Messages/Messages";
 import io from "socket.io-client";
 import Input from "../Input/Input";
-import 'emoji-mart/css/emoji-mart.css'
-import onlineIcon from '../../icons/onlineIcon.png';
-import Fade from 'react-reveal/Fade';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-
-
-
-
+import "emoji-mart/css/emoji-mart.css";
+import onlineIcon from "../../icons/onlineIcon.png";
+import Fade from "react-reveal/Fade";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import "./Chat.css";
 
 let socket;
 let timeout = undefined;
 
-
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
-  const [isTyping, setIsTyping] = useState(false)
+  const [isTyping, setIsTyping] = useState(false);
   const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const [users, setUsers] = useState('');
-  const [isEmoji, setIsEmoji] = useState(false)
-  const [msg, setMsg] = useState('')
-  const [typingUser, setTypingUser] = useState("")
-  const [isToggle, setIsToggle] = useState(false)
-  const [isCopied, setIsCopied] = useState('')
-
-
-
-
+  const [users, setUsers] = useState("");
+  const [isEmoji, setIsEmoji] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [typingUser, setTypingUser] = useState("");
+  const [isToggle, setIsToggle] = useState(false);
+  const [isCopied, setIsCopied] = useState("");
 
   const ENDPOINT = "https://buzz-and-go.herokuapp.com/";
   //const ENDPOINT = "http://localhost:5000/"
@@ -57,75 +48,71 @@ const Chat = ({ location }) => {
       setMessages([...messages, message]);
     });
     //for istyping
-    socket.on('display', (data) => {
+    socket.on("display", (data) => {
       if (data.typing === true) {
-        setIsTyping(true)
-        setTypingUser(data.user)
+        setIsTyping(true);
+        setTypingUser(data.user);
+      } else {
+        setIsTyping(false);
       }
-      else {
-        setIsTyping(false)
-      }
-    })
+    });
 
     socket.on("roomData", ({ users, room }) => {
       setUsers(users);
     });
   }, [messages, name]);
 
-
   const sendMessage = (event) => {
     event.preventDefault();
     if (message) {
-      setIsTyping(false)
+      setIsTyping(false);
       socket.emit("sendMessage", message, () => setMessage(""));
     }
   };
 
   // for emoji
   const handleEmoji = () => {
-    setIsEmoji(!isEmoji)
-  }
+    setIsEmoji(!isEmoji);
+  };
   const addEmoji = (emoji) => {
-    const text = `${msg}${emoji.native}`
-    setMsg(text)
+    const text = `${msg}${emoji.native}`;
+    setMsg(text);
     setMessage(text);
     // setIsEmoji(false)
-  }
+  };
 
   //for typing
   const typingTimeout = () => {
-    socket.emit('typing', { user: name, typing: false })
-    setIsTyping(false)
-  }
+    socket.emit("typing", { user: name, typing: false });
+    setIsTyping(false);
+  };
   const handleKeydown = (e) => {
     if (e.keyCode !== 13) {
-      socket.emit('typing', { user: name, typing: true })
-      clearTimeout(timeout)
-      timeout = setTimeout(typingTimeout, 4000)
+      socket.emit("typing", { user: name, typing: true });
+      clearTimeout(timeout);
+      timeout = setTimeout(typingTimeout, 4000);
+    } else {
+      clearTimeout(timeout);
+      typingTimeout();
     }
-    else {
-      clearTimeout(timeout)
-      typingTimeout()
-
-    }
-  }
+  };
   //for copying
   const handleCopy = (e) => {
-    setIsCopied(e)
+    setIsCopied(e);
     setTimeout(() => {
-      setIsCopied('')
+      setIsCopied("");
     }, 3000);
-  }
+  };
 
   return (
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} setIsToggle={setIsToggle} isToggle={isToggle} />
 
-        {
-          isToggle ? (users
-            ? (<Fade right cascade >
-              <div className={`onlinePeople ${isToggle ? "" : ""}`} >
+        {isToggle ? (
+          users ? (
+            <Fade right cascade>
+              <div className={`onlinePeople ${isToggle ? "" : ""}`}>
                 <h2>
                   {users.map(({ name }) => (
                     <div key={name} className="activeItem">
@@ -134,17 +121,22 @@ const Chat = ({ location }) => {
                     </div>
                   ))}
                 </h2>
-                <CopyToClipboard className="inviteBtn" text={`Hey! Let's chat on https://buzz-and-go.herokuapp.com/. Join my temporary room "${room}" and we're good to go.`}>
-                  <button onClick={() => handleCopy('invite')}>{isCopied === 'invite' ? "Copied!" : "Invite link"}</button>
+                <CopyToClipboard
+                  text={`Hey! Let's chat on https://buzz-and-go.herokuapp.com/. Join my temporary room "${room}" and we're good to go.`}
+                >
+                  <button
+                    className={
+                      isCopied === "invite" ? "copiedBtn" : "inviteBtn"
+                    }
+                    onClick={() => handleCopy("invite")}
+                  >
+                    {isCopied === "invite" ? "Copied!" : "Invite link"}
+                  </button>
                 </CopyToClipboard>
-
               </div>
             </Fade>
-
-            )
-            : null) : (null)
-        }
-
+          ) : null
+        ) : null}
 
         <Messages messages={messages} name={name} />
 
@@ -153,7 +145,9 @@ const Chat = ({ location }) => {
           title='Pick your emoji…' emoji='point_up'
           style={{ position: 'relative' }}
           i18n={{ search: 'Recherche', categories: { search: 'Résultats de recherche', recent: 'Récents' } }} />) : (null)} */}
-        {(name === typingUser ? (null) : (isTyping ? (<h5 className="typingMsg">{typingUser} is typing..</h5>) : (null)))}
+        {name === typingUser ? null : isTyping ? (
+          <h5 className="typingMsg">{typingUser} is typing..</h5>
+        ) : null}
         <Input
           message={message}
           setMessage={setMessage}
