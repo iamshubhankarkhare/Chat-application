f5d8f4e519fe867dffa99749d1a8867dba5b4d34
Pratik Agarwal <pratik1234agarwal@gmail.com>
2021-01-03 00:20:23 +0530

    Added Option for the user to enter the code, to enter a private room

--- server/router.js
+++ server/router.js
@@ -1,9 +1,17 @@
-// const express=require('express')
-// const router=express.Router();
+const express = require('express');
+const router = express.Router();
+const { rooms } = require('./users');
 
+// @route - /api/getRoomDetails/:room
+// desc - Get the room details of the specified room name
+router.get('/getRoomDetails/:room', (req, res) => {
+  const roomName = req.params.room;
+  let index = rooms.findIndex((target) => target.room === roomName);
+  let room = rooms[index];
+  if (!room) {
+    room = {};
+  }
+  res.json(room);
+});
 
-// router.get('/',(req,res)=>{
-//     res.send("server running mf")
-// })
-
-// module.exports=router;
+module.exports = router;
